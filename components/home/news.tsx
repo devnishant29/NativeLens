128a6248525231047e2f6b5e7d8519bbb63fba5e
devnishant29 <devnihsant29@gmail.com>
2025-04-08 21:44:36 +0530

    added images for news section

--- components/home/news.tsx
+++ components/home/news.tsx
@@ -1,25 +1,30 @@
 import { Text, View } from "react-native";
+import { useState } from "react";
 import React from "react";
 import { Image } from "react-native";
 
 import Animated, { FadeInDown } from "react-native-reanimated";
 
 const News = () => {
+  const [imageError, setImageError] = useState(false);
   const newsItems = [
     {
       title:
         "This superstar was Ratan Tata's closest friend, shared same room, went for picnics, listened songs toge...",
-      imageUrl: "https://placehold.co/400x300/jpg",
+      imageUrl:
+        "https://images.pexels.com/photos/4417069/pexels-photo-4417069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       title:
         "This superstar was Ratan Tata's closest friend, shared same room, went for picnics, listened songs toge...",
-      imageUrl: "https://placehold.co/400x300/jpg",
+      imageUrl:
+        "https://images.pexels.com/photos/3593923/pexels-photo-3593923.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       title:
         "This superstar was Ratan Tata's closest friend, shared same room, went for picnics, listened songs toge...",
-      imageUrl: "https://placehold.co/400x300/jpg",
+      imageUrl:
+        "https://images.pexels.com/photos/2876511/pexels-photo-2876511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
   ];
   return (
@@ -30,11 +35,23 @@ const News = () => {
           className="mx-4 mb-4 rounded-xl overflow-hidden"
           entering={FadeInDown.duration(500).delay(index * 100)}
         >
-          <Image
-            source={{ uri: item.imageUrl }}
-            className="w-full h-60 rounded-[24px]"
-            resizeMode="cover"
-          />
+          {imageError ? (
+            <View
+              style={{
+                width: "100%",
+                backgroundColor: "gray",
+                borderRadius: 8,
+              }}
+            />
+          ) : (
+            <Image
+              source={{ uri: item.imageUrl }}
+              className="w-full h-60 rounded-[24px]"
+              resizeMode="cover"
+              onError={() => setImageError(true)}
+            />
+          )}
+
           <View className="px-2 pb-6 pt-2">
             <Text className="text-white text-xl font-medium" numberOfLines={3}>
               {item.title}
